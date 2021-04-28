0525ad44a29de6ce62a50ddbbca241c02087a6dd
CHADAD <mohamed.chadad@nimbleways.com>
2021-04-28 12:14:35 +0000

    add CRUD routes

--- src/main/java/com/example/speedco/DAO/repositories/UserRepository.java
+++ src/main/java/com/example/speedco/DAO/repositories/UserRepository.java
@@ -1,9 +1,22 @@
 package com.example.speedco.DAO.repositories;
 
 import com.example.speedco.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaSpecificationExecutor<User> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findById(Long id);
+
+    List<User> findAllByEmail(String email);
+
+    List<User> findAllByName(String name);
+
+    // here we combine to conditions
+    List<User> findAllByNameAndEmail(String name, String email);
 }
--- src/main/java/com/example/speedco/controllers/UserController.java
+++ src/main/java/com/example/speedco/controllers/UserController.java
@@ -1,25 +1,64 @@
 package com.example.speedco.controllers;
 
+import com.example.speedco.DAO.repositories.UserRepository;
 import com.example.speedco.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping
     public List<User> getUsers() {
         return null;
     }
 
+    // create a filter object
     @PostMapping("/filter")
     public Page<User> getFilteredUsers() {
         return null;
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        return ResponseEntity.ok(userRepository.save(user));
+    }
+
+    @PutMapping("/id")
+    public ResponseEntity<User> create(@RequestBody User user, @PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        user.setId(id);
+        return ResponseEntity.ok(userRepository.save(user));
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        if (!userRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        userRepository.deleteById(id);
+        return ResponseEntity.ok(String.format("User with id: %s successfully deleted", id));
     }
 }
