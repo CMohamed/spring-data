c6647ddaea6a52b641679833cc78dad48ca91cf0
CHADAD <mohamed.chadad@nimbleways.com>
2021-04-29 22:10:39 +0000

    add UserSpecification

---
+++ src/main/java/com/example/speedco/DAO/specifications/UserSpecification.java
@@ -0,0 +1,36 @@
+package com.example.speedco.DAO.specifications;
+
+import com.example.speedco.DAO.utils.SearchCriteria;
+import com.example.speedco.entities.User;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class UserSpecification implements Specification<User> {
+
+    private SearchCriteria criteria;
+
+    @Override
+    public Predicate toPredicate(Root<User> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+        if (criteria.getOperation().equalsIgnoreCase(">")) {
+            return builder.greaterThanOrEqualTo(
+                    root.<String> get(criteria.getField()), criteria.getValue().toString());
+        }
+        else if (criteria.getOperation().equalsIgnoreCase("<")) {
+            return builder.lessThanOrEqualTo(
+                    root.<String> get(criteria.getField()), criteria.getValue().toString());
+        }
+        else if (criteria.getOperation().equalsIgnoreCase(":")) {
+            if (root.get(criteria.getField()).getJavaType() == String.class) {
+                return builder.like(
+                        root.<String>get(criteria.getField()), "%" + criteria.getValue() + "%");
+            } else {
+                return builder.equal(root.get(criteria.getField()), criteria.getValue());
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/com/example/speedco/DAO/utils/SearchCriteria.java
+++ src/main/java/com/example/speedco/DAO/utils/SearchCriteria.java
@@ -2,6 +2,30 @@ package com.example.speedco.DAO.utils;
 
 public class SearchCriteria {
     private String field;
-    private String operation;
+    private String operation; // >, < and : (like)
     private Object value;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
 }
